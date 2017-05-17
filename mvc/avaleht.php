a0d191195d3597a4485b9af591f81228133d8975
andrei <andrei>
2017-05-17 13:33:46 +0300

    mvs töötab, html failid on ümber tehtud php failideks

---
+++ mvc/avaleht.php
@@ -0,0 +1,4 @@
+<div class = "main-body">
+	<p>See leht on tehtud projektina aines "Infosüsteemide analüüs ja projekteerimine". Rühmas on üks inimene : Andrei Pugatšov.</p>
+	<p>Projekti sisuks on personaliotsingu firma uue CRM-i projekteerimine.</p>
+</div>
---
+++ mvc/erd.php
@@ -0,0 +1,8 @@
+<div class = "main-body">
+	<p>Organisatsiooni ERD</p>
+	<p><img src="pics/organisatsiooni_erd.png" alt="Organisatsiooni ERD"></p>
+
+	<p></p>
+	<p>Projekti ERD</p>
+	<p><img src="pics/projekti_erd.png" alt="Projekti ERD"></p>
+</div>
